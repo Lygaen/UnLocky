731f035c9d5594ea1d2f9b222bba8e1f741f9fa7
Lygaen <smalahcayeux@gmail.com>
2020-05-15 18:42:31 +0200

    Update

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import os
 import sys
 
 PORT = 5050
@@ -6,25 +7,26 @@ SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 HEADER = 64
 FORMAT = 'utf-8'
+try:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+    def send(message):
+        msg = message.encode(FORMAT)
+        msg_length = len(msg)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b' ' * (HEADER - len(send_length))
+        client.send(send_length)
+        client.send(msg)
 
-
-def send(message):
-    msg = message.encode(FORMAT)
-    msg_length = len(msg)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(msg)
-
-connected = True
-while connected:
-    input_ = input()
-    send(input_)
-    if input_ == 'quit':
-        connected = False
-sys.exit()
-    
-    
+    connected = True
+    while connected:
+        input_ = input()
+        send(input_)
+        if input_ == 'quit':
+            connected = False
+    sys.exit()
+except ConnectionRefusedError:
+    os.system('cls')
+    print("Il faut toujours allumer le serveur d'abord ! Sinon ça ne marche pas")
+    os.system('pause>nul')
